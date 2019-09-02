ea7082886b56dfaceda79179237c0716e6967933
Sathler <Vinicius.sathler3@yahoo.com.br>
2019-09-01 23:37:54 -0300

    Exe2

--- exercicios/exe2-29-08.hs
+++ exercicios/exe2-29-08.hs
@@ -24,6 +24,10 @@ media_da_lista lista = (foldr (+) 0.0 lista)/fromIntegral(length lista)
 notas1::[Float]
 notas1 = map nota1 turma1
 
+-- Lista das segundas notas da turma
+notas2::[Float]
+notas2 = map nota2 turma1 
+
 --funcao que retorna todos os alunos que tiraram acima de 8
 oitos::[(String,Float,Float)]->[String]
 oitos lista = map nome (filter (\(_,_,n2)->n2>=8)(filter (\(_,n1,_)->n1>=8) lista))
@@ -32,6 +36,18 @@ oitos lista = map nome (filter (\(_,_,n2)->n2>=8)(filter (\(_,n1,_)->n1>=8) list
 todas :: [(String,Float,Float)] -> (Float, Float, Float)
 todas turma = (media_da_lista(map nota1 turma),media_da_lista(map nota2 turma), media_da_lista(map media turma1))
 
+--lista de notas abaixo de 5 da turma
+baixas:: [Float]
+baixas =  filter (<=5.0) (notas1 ++ notas2)
 
+--funcao que retorna a situacao do aluno
+situacao::(String, Float, Float) -> String
+situacao aluno = if (media aluno) >= 5.0 then "Aprovado" else "Reprovado"
 
-main = print (todas turma1)
+linha::(String, Float, Float) -> String
+linha aluno = (nome aluno) ++" "++ show (media aluno) ++ " (" ++ (situacao aluno) ++ ")"
+
+pretty :: [ (String,Float,Float) ] -> String
+pretty turma = foldr (\x y -> concat [x,"\n",y]) " " (map linha turma)
+
+main = putStrLn (pretty turma1)
