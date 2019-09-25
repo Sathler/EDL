2fb740ba5a8d2834f89006b3be0cfbff0bb78aa9
Sathler <Vinicius.sathler3@yahoo.com.br>
2019-09-25 16:51:11 -0300

    lista2-arvores-mapA

--- exercicios/Lista2-Arvores/Ex4.hs
+++ exercicios/Lista2-Arvores/Ex4.hs
@@ -6,6 +6,13 @@ a2 = Galho 5 (Galho 4 (Galho 3 Folha Folha) Folha) Folha
 a3 = Galho 5 Folha (Galho 7 (Galho 6 Folha Folha) (Galho 8 Folha Folha))
 a4 = Galho "batata-frita" (Galho "arroz" Folha Folha) (Galho "feijao" Folha Folha)
 
+--teste converte string
+a5 = Galho 107 (Galho 55 Folha Folha) (Galho 1223 Folha Folha)
+--teste eh primo
+a6 = Galho 3 (Galho 7 (Galho 10 Folha Folha) (Galho 11 Folha Folha))(Galho 15 (Galho 12 Folha Folha) (Galho 19 Folha Folha))
+--teste criaGalho
+a7 = Galho 3 Folha Folha
+
 folhas :: Arvore a -> Int
 folhas Folha = 1
 folhas (Galho _ a1 a2) = folhas a1 + folhas a2
@@ -45,5 +52,33 @@ mapA :: (a -> b) -> Arvore a -> Arvore b
 mapA f Folha = Folha
 mapA f (Galho x a1 a2) = (Galho (f x)(mapA f a1)(mapA f a2))
 
+converteParaString :: Int -> [Char]
+converteParaString x = case x of
+  0 -> "zero"
+  1 -> "um"
+  2 -> "dois"
+  3 -> "tres"
+  4 -> "quatro"
+  5 -> "cinco"
+  6 -> "seis"
+  7 -> "sete"
+  8 -> "oito"
+  9 -> "nove"
+  _ -> converteParaString (div x 10) ++ " " ++ converteParaString(mod x 10)
 
-main = print (mapA even a1)
+--main = print (mapA converteParaString a5)
+
+eh_primo :: Int -> Bool
+eh_primo n = if (fatores n) == [1,n] then True else False
+  where f x = ((mod n x) == 0)
+        fatores n = filter f [1..n]
+        
+
+--main = print (mapA eh_primo a6)
+
+addArvore :: Int -> Arvore Int
+addArvore x = criaArvore x
+  where criaArvore x = if (x == 1) then (Galho 1 Folha Folha) else Galho 1 (criaArvore (x-1)) (criaArvore (x-1))
+
+main = print (mapA addArvore a7)
+
